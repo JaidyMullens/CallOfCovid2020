9cffc424597fe58a25a10efd97784cd604125832
Lucvds99 <63397031+Lucvds99@users.noreply.github.com>
2020-09-04 17:30:25 +0200

    player movment
    i put the player movment system in with jump awsd movment camera control and R for sprint

--- CallOfCovid/Assets/Scripts/SimpleCameraController.cs
+++ CallOfCovid/Assets/Scripts/SimpleCameraController.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 
 namespace UnityTemplateProjects
 {
-    public class SimpleCameraController : MonoBehaviour
+    public class PlayerCameraController : MonoBehaviour
     {
         class CameraState
         {
---
+++ TeachingTimeWithJaidy/Assets/Scripts/TextFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextFollow : MonoBehaviour
+{
+    
+        
+        public GameObject player;
+
+        public Transform TextTransform;
+
+        public Vector3 offset;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            TextTransform = this.GetComponent<Transform>();
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            TextTransform.position = player.transform.position + offset;
+        }
+    
+
+}
