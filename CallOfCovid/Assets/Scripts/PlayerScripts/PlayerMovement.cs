05adbd134a0c6d69084c9c3c38ef3dd5ebab5d31
Jaidy Mullens <jaidymullens12@gmail.com>
2020-09-07 11:17:42 +0200

    Scripts Georderd

--- CallOfCovid/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ CallOfCovid/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -23,15 +23,7 @@ public class PlayerMovement : MonoBehaviour
 
     bool isSprinting;
 
-    
-    
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
