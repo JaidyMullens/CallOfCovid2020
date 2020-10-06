4cc09836f85e64956b6826826c3c12cc3c7ed6db
Martin Tax <54533886+keso689@users.noreply.github.com>
2020-10-06 12:05:48 +0200

    Shotgun materials

---
+++ CallOfCovid/Assets/Scripts/GunScripts/Shooting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public KeyCode shootKey = KeyCode.F;
+    public GameObject projectile;
+    public float shootForce;
+    Transform shootingPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+        shootingPoint = GameObject.Find("shootingPoint").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(shootKey))
+        {
+            GameObject shot = GameObject.Instantiate(projectile, shootingPoint.position, transform.rotation);
+            shot.GetComponent<Rigidbody>().AddForce(transform.forward * shootForce);
+        }
+    }
+}
