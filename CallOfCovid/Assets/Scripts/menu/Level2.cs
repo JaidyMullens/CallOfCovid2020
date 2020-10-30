53b5cbb307993e0ea3d8b726bdb90700067a4927
Brae <brae30.09.2003@gmail.com>
2020-10-30 14:37:57 +0100

    specialedd

---
+++ CallOfCovid/Assets/Scripts/menu/Level2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level2 : MonoBehaviour
+{ 
+    public void level2()
+    {
+        SceneManager.LoadScene("Level 02", LoadSceneMode.Single);
+    }
+}
